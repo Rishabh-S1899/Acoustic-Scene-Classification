e168f891c64b90419db412c32023ad4d8f70f6a0
Rishabh Shukla <126354397+Rishabh-S1899@users.noreply.github.com>
2024-05-24 15:30:20 +0530

    Update Mischallaneous.py

--- PaSST/Mischallaneous.py
+++ PaSST/Mischallaneous.py
@@ -66,4 +66,50 @@ def one_hot(lst,run_mode='scene'):
     enc=OneHotEncoder()
     encoded_lst = enc.fit_transform(label_array).toarray()
     return encoded_lst
+def load_audio_to_tensor(audio_path):
+    y, sr = librosa.load(audio_path, sr=None)
+    y_resampled = librosa.resample(y, orig_sr=sr, target_sr=32000)
+    y_tensor = torch.from_numpy(y_resampled).float()
+    y_tensor = y_tensor.unsqueeze(0)
+    return y_tensor.cuda() 
 
+def generate_embeddings_and_save_to_csv(folder_path, model, device):
+    """Generates embeddings for files in a folder and saves them to a CSV file.
+
+    Args:
+        folder_path (str): Path to the folder containing the files.
+        model: The embedding generation model.
+        device: The device to use for model processing (e.g., "cuda" or "cpu").
+    """
+    save_path="Classification/Embedding_files/PaSST_Embeddings/hear21_embeddings_blocks.csv"
+    embeddings = []
+    with open(fr"Classification/Embedding_files/PaSST_Embeddings/hear21_embeddings_blocks.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["file_name", "embedding1", "embedding2", "embedding3", "embedding4", "embedding5", "embedding6", "embedding7", "embedding8", "embedding9", "embedding10", "embedding11", "embedding12","pre_logits"])
+
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                # Load input data from file_path (replace with your specific logic)
+                val = load_audio_to_tensor(file_path)  # Replace with your loading function
+
+                # Move model and input to CUDA device
+                model = model.to(device)
+                val = val.to(device)
+
+                with torch.no_grad():
+                    # Generate embedding using the model
+                    # embedding = model(val).cpu().detach().numpy().tolist()
+                    embedding=model(val)
+
+                writer.writerow([filename]+embedding[1]+[embedding[0]])
+                print(f"file {filename} encoded")
+
+            except Exception as e:
+                print(f"Error processing {filename}: {e}")
+    return save_path
+            
+def extract_info(filename):
+    filename = filename.split('.')[0] #Remove.wav extension
+    parts = filename.split('-')
+    return {'scene': parts[0], 'location': parts[1], 'device': parts[-1]}
